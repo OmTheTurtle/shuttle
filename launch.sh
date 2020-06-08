9fcf4ada299e47e40fc636f3783ab36b69f746e5
János Fehér <feherjani96@gmail.com>
2020-06-08 14:51:30 +0200

    Copy all config files

--- launch.sh
+++ launch.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
-# Create some basic folders
+echo Create basic folders
 mkdir ~/repos ~/Videos/Movies ~/Videos/Shows
 
-# Install base packages
+echo Installing packages from the arch repository
 sudo pacman -S --noconfirm --needed < packages-repository.txt
 
-# Install packages from AUR
+echo Installing packages from the AUR
 yay -S --needed < packages-AUR.txt
 
-# Clone dotfiles
+echo "Cloning dotfiles to `~/repos/dotfiles`"
 git clone https://github.com/omtheturtle/dotfiles ~/repos/dotfiles
 
-# TODO: copy all the config files to their respective directories
-cp -r ~/repos/dotfiles/.config/. ~/.config
+# Change to dotfiles directory
+cd ~/repos/dotfiles
+
+echo Copying config files...
+cp -r .config/. ~/.config
+cp .vimrc .zshrc .gitconfig ~
+
+# This assumes that the OSS version of VSCode is installed
+# that is available from the arch package repository
+cp .vscode/settings.json ~/.config/Code\ -\ OSS/User/
+
+echo All done!
 
